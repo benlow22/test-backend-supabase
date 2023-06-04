258fb099b1a63d78a0b54b30e1e68f9a8539c331
benlow22 <ben2.2low@gmail.com>
2023-06-03 17:23:56 -0700

    express app listening at 8000

--- back-end/server.js
+++ back-end/server.js
@@ -5,15 +5,11 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 8000;
 
-const workoutsModule = require("./routes/workouts");
-const workoutsRouter = workoutsModule.router;
 app.use(cors());
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
-
-app.use("/workouts", workoutsRouter);
 
 app.listen(port, () => {
 	// eslint-disable-next-line no-console
